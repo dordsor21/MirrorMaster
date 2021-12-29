1ba0f1827a24c9f2165bb607efbd80a0b6207693
dordsor21 <dordsor21@gmail.com>
2021-12-29 17:38:41 +0000

    1.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,34 @@ plugins {
 
 plugins.withId("java") {
     the<JavaPluginExtension>().toolchain {
-        languageVersion.set(JavaLanguageVersion.of(16))
+        languageVersion.set(JavaLanguageVersion.of(17))
         vendor.set(JvmVendorSpec.ADOPTOPENJDK)
     }
 }
 
 repositories {
-    mavenLocal()
+    mavenCentral()
+
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://repo.codemc.io/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+
     maven {
         url = uri("https://ci.athion.net/plugin/repository/tools/")
     }
@@ -21,16 +42,28 @@ repositories {
     }
 
     maven {
+        url = uri("https://mvn.intellectualsites.com/content/repositories/snapshots/")
+    }
+
+    maven {
         url = uri("https://repo.maven.apache.org/maven2/")
+    }
+
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://maven.enginehub.org/repo/")
     }
 }
 
 dependencies {
-    compileOnly("org.spigotmcv1_17_r1_2:spigotmcv1_17_r1_2:1_17_r1_2")
-    compileOnly("com.plotsquared:PlotSquared-Bukkit:6.1.2") {
+    compileOnly("org.spigotmc:spigot-api:1.18-R0.1-SNAPSHOT")
+    compileOnly("com.plotsquared:PlotSquared-Bukkit:6.2.1") {
         isTransitive = false
     }
-    compileOnly("com.plotsquared:PlotSquared-Core:6.1.2")
+    compileOnly("com.plotsquared:PlotSquared-Core:6.2.1")
 }
 
 group = "be.mc.woutwoot"
